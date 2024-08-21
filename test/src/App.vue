34a81de4641fdbaaa66757eefab1aa60c22f85c1
ahmadalanib <alnaib888@gmail.com>
2024-08-21 11:12:07 +0200

    use emits

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,8 +1,13 @@
-
-<script>
+<script setup>
+import EventCard from '@/components/EventCard.vue';
 </script>
 <template>
-  <div>
-    home
+  <div class="container mx-auto my-8 space-y-8 p-5">
+    <h1 class="text-4xl">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <div class="grid grid-cols-2 gap-8">
+      <EventCard title="test" date="30-12-1988" description="dfdf"  @register="console.log('dfdf')"/>
+    </div>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
   </div>
 </template>
---
+++ test/src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Button from '@/components/Button.vue';
+
+
+
+defineProps({
+  title: String,
+  description: String,
+  date: String,
+});
+
+
+defineEmits(['register']);
+
+</script>
+
+<template>
+  <div class="border border-gray-200 bg-white rounded-md">
+ <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+ <p class="p-4 border-b border-gray-200">{{ date }}</p>
+ <p class="p-4">{{description}}</p>
+ <section class="flex justify-end p-4">
+
+  <Button label="Register" @click="$emit('register')"/>
+
+ </section>
+  </div>
+  
+</template>
