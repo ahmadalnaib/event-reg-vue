e1f972edc6e57d92963c63b8a2c062c957ec6cf1
ahmadalanib <alnaib888@gmail.com>
2024-08-22 13:29:34 +0200

    postion data

--- test/src/App.vue
+++ test/src/App.vue
@@ -18,6 +18,28 @@ const fetchEvents = async () => {
   }
 };
 
+const handleREgistration=async(event)=>{
+  const newBooking={
+    id:Date.now().toString(),
+    userId:1,
+    eventId:event.id,
+    eventTitle:event.title,
+
+  }
+  await fetch('http://localhost:3001/bookings',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+      ...newBooking,
+      status:'confirmed'
+
+    })
+  })
+ 
+}
+
 onMounted(fetchEvents);
 </script>
 <template>
@@ -32,7 +54,7 @@ onMounted(fetchEvents);
           :title="event.title"
           :date="event.date"
           :description="event.description"
-          @register="console.log('dfdf')"
+          @register="handleREgistration(event)"
         />
       </template>
       <template v-else>
